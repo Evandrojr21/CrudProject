9e44a775aa16f1f2ec24a7a70792d539850ac6c2
Evandro Santos <evandro.santos@dowjones.com>
2025-02-28 10:04:32 -0300

    Update Controller

--- src/main/java/com/example/SpringRestApi/controller/StudentController.java
+++ src/main/java/com/example/SpringRestApi/controller/StudentController.java
@@ -1,10 +1,18 @@
 package com.example.SpringRestApi.controller;
 
+import com.example.SpringRestApi.dto.StudentDto;
 import com.example.SpringRestApi.entity.Student;
-import com.example.SpringRestApi.exception.StudentNotFoundException;
+import com.example.SpringRestApi.mapper.StudentConvert;
 import com.example.SpringRestApi.repository.StudentRepository;
+import com.example.SpringRestApi.service.StudentService;
+import com.example.SpringRestApi.sqs.model.StudentUpdateMessage;
+import com.example.SpringRestApi.sqs.model.UpdateType;
+import com.example.SpringRestApi.sqs.service.MessageSender;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import java.util.List;
+import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -22,41 +30,66 @@ import org.springframework.web.bind.annotation.RestController;
     @Autowired
     StudentRepository repo;
 
+    @Autowired
+    MessageSender messageSender;
 
-  @GetMapping("/students")
-  public List<Student> getALStudents(){
-    List<Student> students = repo.findAll();
-      return students;
+    @Autowired
+    StudentService studentService;
+
+    @Autowired
+    StudentConvert studentConvert;
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @GetMapping("/students")
+    public List<StudentDto> getAllStudents() throws JsonProcessingException {
+      List<Student> students = studentService.getAllStudents();
+      List<StudentDto> studentDtos = students.stream()
+          .map(studentConvert::convertStudentToStudentDTO)
+          .collect(Collectors.toList());
+
+      String message = objectMapper.writeValueAsString(new StudentUpdateMessage("All Students Retrieved", null, UpdateType.GET));
+      messageSender.sendMessage(message);
+
+      return studentDtos;
     }
 
-  @GetMapping("/students/{id}")
-  public Student getStudentById(@PathVariable int id){
-    if (repo.findById(id).isEmpty()){
-      throw new StudentNotFoundException("Requested Student doesn't exist");
+    @GetMapping("/students/{id}")
+    public StudentDto getStudentById(@PathVariable int id) throws JsonProcessingException {
+      Student student = studentService.getStudentById(id);
+      StudentDto studentDto = studentConvert.convertStudentToStudentDTO(student);
+
+      StudentUpdateMessage studentGetMessage = new StudentUpdateMessage(student.getName(), student.getBranch(), UpdateType.GET);
+      String stringMessage = objectMapper.writeValueAsString(studentGetMessage);
+      messageSender.sendMessage(stringMessage);
+
+      return studentDto;
     }
-    Student student = repo.findById(id).get();
-      return student;
-  }
 
-  @PostMapping("/student/add")
-  @ResponseStatus(code = HttpStatus.CREATED)
-  public void createStudent(@RequestBody @Valid Student student){
-    repo.save(student);
-  }
+    @PostMapping("/students/")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createStudent(@RequestBody @Valid StudentDto studentDto) throws JsonProcessingException {
+      Student student = studentConvert.convertStudentDTOtoStudent(studentDto);
+      studentService.createStudent(student);
+    }
 
-  @PutMapping("/student/update/{id}")
-  public Student updateStudents(@PathVariable int id){
-    Student student = repo.findById(id).get();
-    student.setName("loremipsum");
-    student.setPercentage(92);
-    repo.save(student);
-    return student;
-  }
+    @PutMapping("/students/{id}")
+    public StudentDto updateStudent(@PathVariable int id, @RequestBody StudentDto studentDto) throws JsonProcessingException {
+      Student student = studentConvert.convertStudentDTOtoStudent(studentDto);
+      Student updatedStudent = studentService.updateStudent(id, student);
 
-  @DeleteMapping("/student/delete/{id}")
-  public void removeStudent(@PathVariable int id){
-  Student student = repo.findById(id).get();
-  repo.delete(student);
+      StudentDto updatedStudentDto = studentConvert.convertStudentToStudentDTO(updatedStudent);
+
+      StudentUpdateMessage studentUpdateMessage = new StudentUpdateMessage(updatedStudentDto.getName(), updatedStudentDto.getBranch(), UpdateType.UPDATE);
+      String stringMessage = objectMapper.writeValueAsString(studentUpdateMessage);
+      messageSender.sendMessage(stringMessage);
+
+      return updatedStudentDto;
+    }
+
+  @DeleteMapping("/students/{id}")
+  public void removeStudent(@PathVariable int id) throws JsonProcessingException {
+    studentService.deleteStudent(id);
   }
 
 }
