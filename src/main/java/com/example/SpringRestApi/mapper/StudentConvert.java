79f2730fbe0007a830efcd8674aabce18d043ddb
Evandro Santos <evandro.santos@dowjones.com>
2025-02-28 10:04:48 -0300

    adding StudentConvert

---
+++ src/main/java/com/example/SpringRestApi/mapper/StudentConvert.java
@@ -0,0 +1,25 @@
+package com.example.SpringRestApi.mapper;
+
+import com.example.SpringRestApi.dto.StudentDto;
+import com.example.SpringRestApi.entity.Student;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentConvert {
+  @Autowired
+  private ModelMapper modelMapper;
+
+  public StudentDto convertStudentToStudentDTO(Student student){
+
+    StudentDto studentDto =  modelMapper.map(student,StudentDto.class);
+        return studentDto;
+  }
+
+  public Student convertStudentDTOtoStudent(StudentDto studentDto){
+    Student student =  modelMapper.map(studentDto,Student.class);
+    return student;
+  }
+
+}
