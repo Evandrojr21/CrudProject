116df5e684bf0b504fd8b0be8374dca7cf158025
Evandro Santos <evandro.santos@dowjones.com>
2025-02-28 10:01:52 -0300

    Update the student model

--- src/main/java/com/example/SpringRestApi/entity/Student.java
+++ src/main/java/com/example/SpringRestApi/entity/Student.java
@@ -6,11 +6,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name = "student")
@@ -20,22 +15,17 @@ public class Student {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
 
-  @NotEmpty(message = "The Student name cannot be empty")
-  @Size(min = 3, max = 15, message = "Invalid name")
   @Column
   private String name;
 
-
-  @Min(value = 0, message = "the percentage cannot be less than 0 ")
-  @Max(value = 100, message = "the percentage cannot be greater than 100 ")
   @Column
   private float percentage;
 
-  @NotNull(message = "The student branch cannot be null")
   @Column
   private String branch;
 
-  public Student() {
+  public Student(){
+
   }
 
   public Student(Student studentData){
@@ -49,6 +39,7 @@ public class Student {
     this.percentage = percentage;
     this.branch = branch;
   }
+  
 
   public int getId() {
     return id;
